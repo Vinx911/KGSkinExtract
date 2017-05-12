3e18a2eba3475c337b9fa776930e8c409c20d7bd
VIIs <o7thzhan@163.com>
2017-05-12 12:01:06 +0800

    添加打包

---
+++ KGSkinExtract/ResExtract.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace KugouSkinRead
+{
+    struct KugouResRec 
+    {
+        public int Index;
+        public int FileSize;
+        public String FileName;
+        public Int64 Position;
+        public Byte[] Other;
+    };
+
+    class ResExtract
+    {
+        public List<KugouResRec> FileList;
+        public String FileName;
+        public BinaryReader FMemStream;
+
+        //委托
+        public delegate void dExtractProgress(long total, long current, String filename);
+        //事件
+        public event dExtractProgress onExtractProgress;
+
+        public ResExtract()
+        {
+            FileList = new List<KugouResRec>();
+        }
+
+        public void clear()
+        {
+            FileList.Clear();
+        }
+
+        public void Destroy()
+        {
+            FMemStream.Close();
+        }
+
+        public void LoadFromFile(String FileName)
+        {
+            int         FileCount;
+            int         RecLen;
+            Byte        NameLen;
+            Byte[]      bName;
+            Int64       FileStartPosi;
+            FileStream  fs;
+            KugouResRec Item;
+
+            Item = new KugouResRec();
+            try
+            {
+                clear();
+                this.FileName = FileName;
+                fs = File.Open(FileName, FileMode.Open);
+                this.FMemStream = new BinaryReader(fs);
+                FileCount = this.FMemStream.ReadInt32();
+
+                for (int i = 0; i < FileCount; i++)
+                {
+                    Item.Index = i;
+                    RecLen = this.FMemStream.ReadInt32();
+                    NameLen = this.FMemStream.ReadByte();
+                    bName = this.FMemStream.ReadBytes(NameLen);
+                    Item.FileName = System.Text.Encoding.Unicode.GetString(bName);
+                    Item.Other = this.FMemStream.ReadBytes(RecLen - (4 + 1 + NameLen) - 4);
+                    Item.FileSize = this.FMemStream.ReadInt32();
+                    Item.Position = this.FMemStream.BaseStream.Position;
+                    FileList.Add(Item);
+                    if(i> 0x6aa)
+                    {
+                        int jj = i;
+                    }
+                }
+                FileStartPosi = this.FMemStream.BaseStream.Position;
+                for (int i = 0; i < FileList.Count; i++)
+                {
+                    Item = FileList[i];
+                    Item.Position = FileStartPosi;
+                    FileStartPosi += Item.FileSize;
+                    FileList[i] = Item;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        public void Extract(int Index, byte[] bd)
+        {
+            KugouResRec Item;
+            if(Index >=0 && Index < FileList.Count)
+            {
+                Item = FileList[Index];
+                //bd = new byte[Item.FileSize];
+                FMemStream.BaseStream.Seek(Item.Position, SeekOrigin.Begin);
+                FMemStream.Read(bd, 0, Item.FileSize);
+            }
+        }
+
+        public void Extract(String filename, byte[] bd)
+        {
+            for (int i = 0; i < FileList.Count;i++)
+            {
+                if(String.Compare(FileList[i].FileName, filename, true) == 0)
+                {
+                    Extract(i,bd);
+                }
+            }
+        }
+
+        public void ExtractAll(String fileDir)
+        {
+            try
+            {
+                if (!Directory.Exists(fileDir))
+                {
+                    Directory.CreateDirectory(fileDir);
+                }
+                if (fileDir.LastIndexOf('\\') != fileDir.Length - 1)
+                {
+                    fileDir += "\\";
+                }
+                for (int i = 0; i < FileList.Count; i++)
+                {
+                    byte[] bd = new byte[FileList[i].FileSize];
+                    Extract(i, bd);
+
+                    String FileName = fileDir + FileList[i].FileName;
+
+                    FileStream fs = File.Create(FileName);
+                    BinaryWriter bw = new BinaryWriter(fs);
+                    bw.Write(bd, 0, FileList[i].FileSize);
+                    fs.Close();
+
+                    if (onExtractProgress != null)
+                        onExtractProgress(FileList.Count, i, FileName);
+                }
+            }
+            catch(Exception){}
+        }
+    }
+}
---
+++ KGSkinExtract/ResPackage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics; 
+
+namespace KugouSkinRead
+{
+    class ResPackage
+    {
+        public String Path;
+        public ArrayList FileList;
+        private DirectoryInfo folder;
+
+        //委托
+        public delegate void dPackageProgress(long total, long current, String filename);
+        //事件
+        public event dPackageProgress onPackageProgress;
+
+        public ResPackage(String Path)
+        {
+            if ((Path.LastIndexOf('\\') != Path.Length - 1) && (Path.LastIndexOf('/') != Path.Length - 1))
+            {
+                Path += "\\";
+            }
+            this.Path = Path;
+            this.folder = new DirectoryInfo(Path);
+            this.FileList = GetFileNames(folder);
+        }
+
+        public void Pack(String FileName)
+        {
+            FileStream fs;
+            BinaryWriter FMemStream;
+            String[] fl = (String[])this.FileList.ToArray(typeof(String));
+            Int32 Position = 0;
+
+            fs = File.Create(FileName);
+            FMemStream = new BinaryWriter(fs);
+            FMemStream.Write((Int32)fl.Length);
+
+            String dat = System.Environment.GetEnvironmentVariable("TEMP") + "\\~res.dat";
+            String dat2 = System.Environment.GetEnvironmentVariable("TEMP") + "\\~res2.dat";
+
+            Tools.CmdHelper.ExecuteCmd("echo a 2>\"" + dat + "\"");
+
+            long current = 0;
+            foreach (String file in fl)
+            {
+                FileStream resfile = File.Open(this.Path + file,FileMode.Open);
+                FMemStream.Write((Int32)(file.Length * 2 + 13));
+                FMemStream.Write((Byte)(file.Length * 2));
+                FMemStream.Write(System.Text.Encoding.Unicode.GetBytes(file));
+                FMemStream.Write(Position);
+                FMemStream.Write((Int32)resfile.Length);
+                Position += (Int32)resfile.Length;
+                resfile.Close();                
+
+                Tools.CmdHelper.ExecuteCmd("copy /b \"" + dat + "\"+\"" + this.Path + file + "\" \"" + dat2 + "\"");
+                Tools.CmdHelper.ExecuteCmd("DEL \"" + dat + "\"");
+                Tools.CmdHelper.ExecuteCmd("REN \"" + dat2 + "\" \"~res.dat\"");
+                
+                if (onPackageProgress != null)
+                    onPackageProgress(fl.Length, ++current, file);
+            }
+
+            fs.Close();
+
+            Tools.CmdHelper.ExecuteCmd("copy /b \"" + FileName + "\"+\"" + dat + "\" \"" + dat2 + "\"");
+            Tools.CmdHelper.ExecuteCmd("DEL \"" + FileName + "\"");
+            Tools.CmdHelper.ExecuteCmd("MOVE \"" + dat2 + "\" \"" + FileName + "\"");
+   
+        }
+
+        private ArrayList GetFileNames(DirectoryInfo dirInfo)
+        {
+            ArrayList fns = new ArrayList();
+
+            foreach (DirectoryInfo subdir in dirInfo.GetDirectories())
+            {
+                fns.AddRange(GetFileNames(subdir));
+            }
+
+            foreach( FileInfo fi in dirInfo.GetFiles())
+            {
+                fns.Add(fi.FullName.Substring(this.Path.Length));
+            }
+            return fns;
+        }
+    }
+}
